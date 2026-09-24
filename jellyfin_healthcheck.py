14a9625c8ec3beca47dc92b647d5f0c45d3d9924
Stefan Peters <stefandesu@exo.pm>
2026-09-24 18:45:58 +0200

    Update auth header

--- jellyfin_healthcheck.py
+++ jellyfin_healthcheck.py
@@ -54,7 +54,7 @@ def _auth_header(token=None):
     )
     if token:
         h += f', Token="{token}"'
-    return {"X-Emby-Authorization": h}
+    return {"Authorization": h}
 
 
 # ── Logging setup ──────────────────────────────────────────────────────────────
